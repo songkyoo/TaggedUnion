8e536bae228cd48805024a170bd524bfe050c374
Song, Gyu <songkyoo@hotmail.com>
2026-07-05 23:10:12 +0900

    chore: 레퍼런스 타입 테스트 추가

---
+++ TaggedUnion.Tests/Helper.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Immutable;
+using System.Reflection;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace Macaron.TaggedUnion.Tests;
+
+public static class Helper
+{
+    public static void AssertGeneratedCode(string sourceCode, string[] expectedCodes)
+    {
+        var (_, generatedCodes) = CompileAndGetResults<TaggedUnionGenerator>(
+            sourceCode,
+            additionalAssemblies: [typeof(TaggedUnionAttribute).Assembly]
+
+        );
+
+        Assert.That(generatedCodes, Has.Length.EqualTo(expectedCodes.Length));
+
+        foreach (var (generatedCode, index) in generatedCodes.Select((code, index) => (code, index)))
+        {
+            Assert.That(
+                actual: generatedCode.ReplaceLineEndings(),
+                expression: Is.EqualTo(expectedCodes[index].ReplaceLineEndings())
+            );
+        }
+    }
+
+    public static void AssertGeneratedCode(string sourceCode, int sourceIndex, string expected)
+    {
+        var (_, generatedCodes) = CompileAndGetResults<TaggedUnionGenerator>(
+            sourceCode,
+            additionalAssemblies: [typeof(TaggedUnionAttribute).Assembly]
+        );
+
+        Assert.That(
+            actual: generatedCodes[sourceIndex].ReplaceLineEndings(),
+            expression: Is.EqualTo(expected.ReplaceLineEndings())
+        );
+    }
+
+    public static void AssertGeneratedCode(string sourceCode, string expected)
+    {
+        AssertGeneratedCode(sourceCode, sourceIndex: 0, expected);
+    }
+
+    private static (ImmutableArray<Diagnostic> diagnostics, string[] generatedCodes) CompileAndGetResults<T>(
+        string sourceCode,
+        Assembly[]? additionalAssemblies = null
+    ) where T : IIncrementalGenerator, new()
+    {
+        var references = AppDomain
+            .CurrentDomain
+            .GetAssemblies()
+            .Concat(additionalAssemblies ?? [])
+            .Where(assembly => !assembly.IsDynamic && !string.IsNullOrWhiteSpace(assembly.Location))
+            .Select(assembly => MetadataReference.CreateFromFile(assembly.Location))
+            .Cast<MetadataReference>()
+            .ToImmutableArray();
+
+        var syntaxTree = CSharpSyntaxTree.ParseText(sourceCode);
+        var compilation = CSharpCompilation.Create(
+            assemblyName: "Macaron.TaggedUnion.Tests",
+            syntaxTrees: [syntaxTree],
+            references,
+            options: new CSharpCompilationOptions(
+                outputKind: OutputKind.DynamicallyLinkedLibrary,
+                nullableContextOptions: NullableContextOptions.Enable
+            )
+        );
+
+        var generator = new T();
+        var driver = CSharpGeneratorDriver.Create(generator);
+
+        var result = driver.RunGenerators(compilation).GetRunResult().Results.Single();
+        var generatedSources = result.GeneratedSources;
+        var generatedCodes = generatedSources.Select(source => source.SourceText.ToString()).ToArray();
+
+        var allDiagnostics = compilation.GetDiagnostics()
+            .Concat(result.Diagnostics)
+            .ToImmutableArray();
+
+        return (allDiagnostics, generatedCodes);
+    }
+}
