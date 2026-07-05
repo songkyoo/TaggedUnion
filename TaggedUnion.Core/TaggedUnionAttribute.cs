ae6e98c7f9880233c3998e715a083a54fa1b9d27
Song, Gyu <songkyoo@hotmail.com>
2026-07-05 23:10:27 +0900

    feat: TaggedUnionAttribute 추가

---
+++ TaggedUnion.Core/TaggedUnionAttribute.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+
+namespace Macaron.TaggedUnion;
+
+[Conditional("SOURCE_GENERATOR_ONLY")]
+[AttributeUsage(validOn: AttributeTargets.Struct)]
+public sealed class TaggedUnionAttribute : Attribute
+{
+    public IReadOnlyList<Type> Types { get; }
+
+    public TaggedUnionAttribute(Type type1, Type type2)
+    {
+        Types = [type1, type2];
+    }
+
+    public TaggedUnionAttribute(Type type1, Type type2, Type type3)
+    {
+        Types = [type1, type2, type3];
+    }
+
+    public TaggedUnionAttribute(Type type1, Type type2, Type type3, Type type4)
+    {
+        Types = [type1, type2, type3, type4];
+    }
+
+    public TaggedUnionAttribute(Type type1, Type type2, Type type3, Type type4, Type type5)
+    {
+        Types = [type1, type2, type3, type4, type5];
+    }
+
+    public TaggedUnionAttribute(Type type1, Type type2, Type type3, Type type4, Type type5, Type type6)
+    {
+        Types = [type1, type2, type3, type4, type5, type6];
+    }
+
+    public TaggedUnionAttribute(Type type1, Type type2, Type type3, Type type4, Type type5, Type type6, Type type7)
+    {
+        Types = [type1, type2, type3, type4, type5, type6, type7];
+    }
+
+    public TaggedUnionAttribute(Type type1, Type type2, Type type3, Type type4, Type type5, Type type6, Type type7, Type type8)
+    {
+        Types = [type1, type2, type3, type4, type5, type6, type7, type8];
+    }
+}
